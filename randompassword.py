11bac0914b8b0df4628ae8c8f519c4de44df3939
Atom <84083811+AylinCoder22@users.noreply.github.com>
2023-06-30 09:47:09 +0100

    Add files via upload

---
+++ randompassword.py
@@ -0,0 +1,17 @@
+import random
+
+# 2 uper case charracters from A-Z;
+upperCase1= chr(random.randint(65,90))
+upperCase2= chr(random.randint(65,90))
+
+#2 lower case characters from A-Z;
+lowerCase1 = chr(random.randint(97,122))
+lowerCase2 = chr(random.randint(97,122))
+
+# 2 digits from 0-9;
+digit1= chr(random.randint(48,57))
+digit2= chr(random.randint(48,57))
+
+# 2 punctuation signs such as !, ?, “, # etc.
+
+punc1 =  chr(random.randint(48,57))
